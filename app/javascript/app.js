119ea2e9d325a5914872fc52a4879a06ff0b48f2
Caio Teles <caioo.teless@gmail.com>
2023-10-01 09:27:07 -0300

    script validations

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -7,17 +7,72 @@ var app = new Vue({
         showModal(e) {
             this.confirmModal = true;
         },
-        scheduleHour(){
+        scheduleHour() {
             $.ajax({
                 url: "rooms",
                 type: 'GET',
-                success: function(e) {
-                  alert('Ok ajax');
+                success: function (e) {
+                    alert('Ok ajax');
                 },
-                error: function(e) {
-                  alert('Erro ajax');
+                error: function (e) {
+                    alert('Erro ajax');
                 }
             })
+        },
+        renderCalendar() {
+            var initialTimeZone = 'UTC';
+            var timeZoneSelectorEl = document.getElementById('time-zone-selector');
+            var loadingEl = document.getElementById('loading');
+            var calendarEl = document.getElementById('calendar');
+
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                locale: 'pt-br',
+                buttonText: {
+                    today: 'Hoje',
+                    month: 'Mês',
+                    week: 'Semana',
+                    day: 'Hoje',
+                    list: 'Lista'
+                },
+                slotLabelFormat: 'HH:mm',
+                timeZone: 'America/Sao_Paulo',
+                initialView: 'timeGridWeek',
+                firstDay: 1,
+                weekends: false,
+                allDaySlot: false,
+                navLinks: true,
+                editable: true,
+                selectable: true,
+                dayMaxEvents: true,
+                dayHeaderFormat: this.dayHeaderFormatUsingMoment,
+                dayMaxEventRows: true,
+
+                contentHeight: 680,
+                aspectRatio: 2,
+                views: {
+                    week: {
+                        dayHeaderFormat: {
+                            weekday: 'long',
+                        },
+                    },
+                },
+                slotMinTime: "06:00:00",
+                slotMaxTime: "19:00:00",
+
+                loading: function (bool) {
+                    if (bool) {
+                        loadingEl.style.display = 'inline';
+                    } else {
+                        loadingEl.style.display = 'none';
+                    }
+                },
+            });
+
+            calendar.render();
+
         }
+    },
+    mounted() {
+        this.renderCalendar();
     }
 });
--- app/javascript/calendar.js
+++ app/javascript/calendar.js
@@ -1,53 +1,51 @@
-document.addEventListener('DOMContentLoaded', function() {
+// $(document).on('turbolinks:load', function (e) {
+//     var initialTimeZone = 'UTC';
+//     var timeZoneSelectorEl = document.getElementById('time-zone-selector');
+//     var loadingEl = document.getElementById('loading');
+//     var calendarEl = document.getElementById('calendar');
 
-    var initialTimeZone = 'UTC';
-    var timeZoneSelectorEl = document.getElementById('time-zone-selector');
-    var loadingEl = document.getElementById('loading');
-    var calendarEl = document.getElementById('calendar');
+//     var calendar = new FullCalendar.Calendar(calendarEl, {
+//         locale: 'pt-br',
+//         buttonText: {
+//             today: 'Hoje',
+//             month: 'Mês',
+//             week: 'Semana',
+//             day: 'Hoje',
+//             list: 'Lista'
+//         },
+//         slotLabelFormat: 'HH:mm',
+//         timeZone: 'America/Sao_Paulo',
+//         initialView: 'timeGridWeek',
+//         firstDay: 1,
+//         weekends: false,
+//         allDaySlot: false,
+//         navLinks: true,
+//         editable: true,
+//         selectable: true,
+//         dayMaxEvents: true,
+//         dayHeaderFormat: this.dayHeaderFormatUsingMoment,
+//         dayMaxEventRows: true,
 
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-    locale: 'pt-br',
-    buttonText: {
-        today: 'Hoje',
-        month: 'Mês',
-        week: 'Semana',
-        day: 'Hoje',
-        list: 'Lista'
-    },
-    slotLabelFormat: 'HH:mm',
-    timeZone: 'America/Sao_Paulo',
-    initialView: 'timeGridWeek',
-    firstDay: 1,
-    weekends: false, 
-    allDaySlot: false,
-    navLinks: true,
-    editable: true,
-    selectable: true,
-    dayMaxEvents: true, 
-    dayHeaderFormat: this.dayHeaderFormatUsingMoment,
-    dayMaxEventRows: true,
+//         contentHeight: 680,
+//         aspectRatio: 2,
+//         views: {
+//             week: {
+//                 dayHeaderFormat: {
+//                     weekday: 'long',
+//                 },
+//             },
+//         },
+//         slotMinTime: "06:00:00",
+//         slotMaxTime: "19:00:00",
 
-    contentHeight: 680,
-    aspectRatio: 2,
-    views: {
-        week: {
-        dayHeaderFormat: {
-            weekday: 'long',
-        },
-        },
-    },
-    slotMinTime: "06:00:00",
-    slotMaxTime: "19:00:00",
+//         loading: function (bool) {
+//             if (bool) {
+//                 loadingEl.style.display = 'inline';
+//             } else {
+//                 loadingEl.style.display = 'none';
+//             }
+//         },
+//     });
+//     calendar.render();
 
-    loading: function(bool) {
-        if (bool) {
-        loadingEl.style.display = 'inline'; 
-        } else {
-        loadingEl.style.display = 'none'; 
-        }
-    },
-    });
-
-    calendar.render();
-
-});
+// });
